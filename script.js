5d69dd3ce9bb2b04ddd10c205179f762c583e9a8
Gabriela Tomaz <159478686+gabbtomaz@users.noreply.github.com>
2024-04-03 23:46:11 -0300

    correção de bugs

--- script.js
+++ script.js
@@ -2,16 +2,8 @@ function toggleMode() {
   const html = document.documentElement
   html.classList.toggle("light")
 
-  // O algoritmo a ser usado está abaixo mas de forma simplificada a função 'toggle' realiza a ação
-  // if(html.classList.contains('light')){
-  // html.classList.remove('light')
-  // } else {
-  // html.classList.add('light')
-
-  // pegar a tag img
   const img = document.querySelector("#profile img")
 
-  // se tiver no ligth mode, substituir a imagem
   if(html.classList.contains('light')){
     img.setAttribute('src', '/projeto/assets/avatar-light.png')
 
@@ -19,7 +11,6 @@ function toggleMode() {
 
   } 
   
-  // caso não manter a imagem
   else {
     img.setAttribute('src', '/projeto/assets/avatar.png')
   }
